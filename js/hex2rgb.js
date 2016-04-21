d7aa7846ea676ac0ad4f6f7889781f7493e52849
Axel <axel.tonson@hetic.net>
2016-04-21 15:38:06 +0200

    characters number verifaction added

--- js/hex2rgb.js
+++ js/hex2rgb.js
@@ -1,14 +1,36 @@
 document.querySelector('input').focus();
-document.querySelector('#convert_button').addEventListener('click', converter)
+document.querySelector('#convert_button').addEventListener('click', check)
 
+var error = false;
 var index = 0;
+function check() {
+    var inputLength = document.querySelector('input').value.length;
+    check_length(inputLength);
+    if (error === false) {
+        converter();
+    }
+    else {
+        document.querySelector('input').focus();
+    }
+}
+
+function check_length(length) {
+    if (length !== 6) {
+        document.querySelector('p').innerHTML = '6 characters needed ! ex: ffe512';
+        error = true;
+    }
+    else {
+        document.querySelector('p').innerHTML = '';
+        error = false;
+    }
+}
 
 function converter() {
     var fullHColor = document.querySelector('input').value;
     var red = convert_hex_to_dec(fullHColor.slice(0, 2));
     var green = convert_hex_to_dec(fullHColor.slice(2, 4));
     var blue = convert_hex_to_dec(fullHColor.slice(4, 6));
-    print_it(red,green,blue);
+    print_it(red, green, blue);
 }
 
 function convert_hex_to_dec(color) {
@@ -16,14 +38,15 @@ function convert_hex_to_dec(color) {
     return color;
 }
 
-function print_it(red,green,blue) {
-    var flux = '<div class="color'+index+' form">rgb('+red+','+green+','+blue+')</div>';
+function print_it(red, green, blue) {
+    var flux = '<div class="color' + index + ' form">' +
+        'rgb(' + red + ',' + green + ',' + blue + ')' +
+        '</div>';
     document.querySelector('section').innerHTML += flux;
-    document.querySelector('.color'+index).style.background = 'rgb('+red+','+green+','+blue+')';
+    document.querySelector('.color' + index).style.background = 'rgb(' + red + ',' + green + ',' + blue + ')';
     index++;
 }
 
-// TODO : - donner le focus au champs de formulaire au chargement de la page
 // TODO : - faire les vérification sur le champ de formulaire
 // TODO : - faire le bouton reset
 // TODO : - possibilité de faire dec to hex
